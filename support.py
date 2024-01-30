d26a729c24a6365d689e07d215aad315a00c01a9
dungs072 <82323846+dungs072@users.noreply.github.com>
2024-01-30 17:28:18 +0700

    last_5_matches

--- support.py
+++ support.py
@@ -5,20 +5,20 @@ def match_strings(str1, str2, threshold=90):
     similarity_ratio = fuzz.ratio(str1.lower(), str2.lower())
     return similarity_ratio >= threshold
 
-def export_to_csv(file_name: str, cols_name: list[str] , data):
-        """
-        Write data into file with cols_name and custom data
-        """
+# def export_to_csv(file_name: str, cols_name: list[str] , data):
+#         """
+#         Write data into file with cols_name and custom data
+#         """
         
-        assert len(data) != 0, "Your data is empty!"
-        assert len(cols_name) == len(data[0]), "Num of cols_name need to be the same with num of cols in data"
+#         assert len(data) != 0, "Your data is empty!"
+#         assert len(cols_name) == len(data[0]), "Num of cols_name need to be the same with num of cols in data"
 
-        with open(file_name, 'w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
+#         with open(file_name, 'w', newline='', encoding='utf-8') as file:
+#             writer = csv.writer(file)
 
-            writer.writerow(cols_name)
+#             writer.writerow(cols_name)
 
-            writer.writerows(data)
+#             writer.writerows(data)
 
 def read_csv(filename: str) -> list:
     """
@@ -37,17 +37,17 @@ def read_csv(filename: str) -> list:
 
     return data
 
-def calc_team_strength(match_id: str) -> tuple[float, float]:
-    """
-    Returns an estimate of the strength of the home and away teams
-    """
+# def calc_team_strength(match_id: str) -> tuple[float, float]:
+#     """
+#     Returns an estimate of the strength of the home and away teams
+#     """
 
-    h_str = 0
-    a_str = 0
+#     h_str = 0
+#     a_str = 0
 
-    # Add your code here...
+#     # Add your code here...
 
-    return (h_str, a_str)
+#     return (h_str, a_str)
 
 def last_5_matches(team_id: str) -> int:
     """
@@ -60,16 +60,42 @@ def last_5_matches(team_id: str) -> int:
 
     return points
 
-def last_5_matches_at(team_id: str, at_home: bool) -> int:
+def last_5_matches_at(team_id: str, at_home: bool, date:str) -> int:
     """
     Return the team performance (home/away) in last 5 matches
     """
-
-    points = 0
-
-    # Add your code here
-
-    return points
+    temp_list = []
+    data = read_csv('Results/results_1516_2223.csv')
+    for row in data:
+        if row[1]>=date: break
+        if at_home:
+            if(row[2]!=team_id): continue
+            if(row[4]>row[5]):
+                #win
+                temp_list.append(3)
+            elif(row[4]<row[5]):
+                #loss
+                temp_list.append(0)
+                pass
+            elif(row[4]==row[5]):
+                #draw
+                temp_list.append(1)
+        else:
+            if(row[3]!=team_id): continue
+            if(row[4]<row[5]):
+                # win
+                temp_list.append(3)
+            elif(row[4]>row[5]):
+                # loss
+                temp_list.append(0)
+            elif(row[4]==row[5]):
+                #draw
+                temp_list.append(1)
+        
+        if len(temp_list)==6:
+            temp_list.pop(0)
+    if len(temp_list)==0: return 0
+    return sum(temp_list)/len(temp_list)
 
 def head_to_head(home_id: str, away_id: str) -> int:
     """
--- test_2.py
+++ test_2.py
@@ -1,107 +1,21 @@
-data = {
-    "h": {
-        "341628": {
-            "id": "341628",
-            "goals": "2",
-            "own_goals": "0",
-            "shots": "4",
-            "xG": "1.3030972480773926",
-            "time": "88",
-            "player_id": "556",
-            "team_id": "89",
-            "position": "AML",
-            "player": "Marcus Rashford",
-            "h_a": "h",
-            "yellow_card": "0",
-            "red_card": "0",
-            "roster_in": "341631",
-            "roster_out": "0",
-            "key_passes": "0",
-            "assists": "0",
-            "xA": "0",
-            "xGChain": "1.1517746448516846",
-            "xGBuildup": "0.6098462343215942",
-            "positionOrder": "13"
-        },
-        "341629": {
-            "id": "341629",
-            "goals": "1",
-            "own_goals": "0",
-            "shots": "4",
-            "xG": "0.7688590884208679",
-            "time": "90",
-            "player_id": "553",
-            "team_id": "89",
-            "position": "FW",
-            "player": "Anthony Martial",
-            "h_a": "h",
-            "yellow_card": "1",
-            "red_card": "0",
-            "roster_in": "0",
-            "roster_out": "0",
-            "key_passes": "1",
-            "assists": "0",
-            "xA": "0.05561231076717377",
-            "xGChain": "0.9395027160644531",
-            "xGBuildup": "0.11503136157989502",
-            "positionOrder": "15"
-        }
-    },
-    "a": {
-        "341633": {
-            "id": "341633",
-            "goals": "0",
-            "own_goals": "0",
-            "shots": "0",
-            "xG": "0",
-            "time": "90",
-            "player_id": "5061",
-            "team_id": "80",
-            "position": "GK",
-            "player": "Kepa",
-            "h_a": "a",
-            "yellow_card": "0",
-            "red_card": "0",
-            "roster_in": "0",
-            "roster_out": "0",
-            "key_passes": "0",
-            "assists": "0",
-            "xA": "0",
-            "xGChain": "0.04707280918955803",
-            "xGBuildup": "0.04707280918955803",
-            "positionOrder": "1"
-        },
-        "341642": {
-            "id": "341642",
-            "goals": "0",
-            "own_goals": "0",
-            "shots": "2",
-            "xG": "0.08609434962272644",
-            "time": "60",
-            "player_id": "592",
-            "team_id": "80",
-            "position": "AML",
-            "player": "Ross Barkley",
-            "h_a": "a",
-            "yellow_card": "0",
-            "red_card": "0",
-            "roster_in": "341646",
-            "roster_out": "0",
-            "key_passes": "1",
-            "assists": "0",
-            "xA": "0.024473881348967552",
-            "xGChain": "0.11056823283433914",
-            "xGBuildup": "0",
-            "positionOrder": "13"
-        }
-    }
-}
-
-# Extract player_ids
-player_ids = []
-
-for team in data.values():
-    for player_data in team.values():
-        player_ids.append(player_data["player_id"])
-
-print(player_ids)
+import csv
+import support as sp
+new_labels = ['h_poise', 'a_poise']
+csv_file_path = 'Results/results_1516_2223.csv'
+output_csv_file_path = 'final_results/02.csv'
+with open(csv_file_path, 'r') as csv_file, open(output_csv_file_path, 'w', newline='') as output_csv_file:
+    csv_reader = csv.reader(csv_file)
+    csv_writer = csv.writer(output_csv_file)
+    header = next(csv_reader, None)
+    if header:
+        csv_writer.writerow(header + new_labels)
+    count = 0
+    for row in csv_reader:
+        h_p = sp.last_5_matches_at(row[2],True, row[1])
+        a_p = sp.last_5_matches_at(row[3],False,row[1])
+        p_list = [h_p,a_p]
+        new_row = row + p_list
+        csv_writer.writerow(new_row)
+        count+=1
+        print(count)
+print(f'Data written to: {output_csv_file_path}')
