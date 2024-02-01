22334615f20be2ed01d6713cedc258b5fc862309
Eren206 <dinhvanty206@gmail.com>
2024-02-01 21:26:58 +0700

    test-khong tac dong gi

---
+++ Players Overall/test_sort.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Đọc dữ liệu từ file CSV
+file_path = "players_overall_1516_2223.csv"
+df = pd.read_csv(file_path)
+
+# Sắp xếp theo cột "1516"
+df_sorted = df.sort_values(by="1516")
+df_sorted.to_csv("du_lieu_sap_xep.csv", index=False)
