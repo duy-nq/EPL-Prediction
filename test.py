1a4496ac40024473453cce2c7f8b4ee53f335498
Nguyen Duy <146707420+duy-nq@users.noreply.github.com>
2024-04-08 13:55:35 +0700

    Minor Change

--- test.py
+++ test.py
@@ -7,8 +7,17 @@ import pandas as pd
 
 FILENAME = 'training_data.csv'
 
+
+def map_outcome(outcome):
+    if outcome == 'WIN' or outcome == 'DRAW':
+        return 'HAVE POINT'
+    else:
+        return '0 POINT'
+
 def read_data(file: str):
     df = pd.read_csv(file, sep=',')
+
+    df['h_str'] = df['h_str'] + 2
 
     X = df.drop('result', axis=1)
     y = df['result']
@@ -22,13 +31,13 @@ model = RandomForestClassifier(n_estimators=56, random_state=42)
 model_linear = LogisticRegression(multi_class='multinomial', max_iter=1000)
 model_svm = SVC(kernel='poly')
 
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+# model.fit(X_train, y_train)
+# y_pred = model.predict(X_test)
 
-model_linear.fit(X_train, y_train)
-y_pred_linear = model_linear.predict(X_test)
+# model_linear.fit(X_train, y_train)
+# y_pred_linear = model_linear.predict(X_test)
 
 model_svm.fit(X_train, y_train)
 y_pred_svm = model_svm.predict(X_test)
 
-print('SVM:', accuracy_score(y_test, y_pred_svm))
+print('MODEL:', accuracy_score(y_test, y_pred_svm))
